7fe1b40148938a2692911c7bcf90b46bee3790d8
Youness Bettahi Berdai <48675571+yunesss00@users.noreply.github.com>
2021-03-27 14:09:10 +0100

    Update btree_utils.hpp

--- p2/i82bebey/binary_tree/btree_utils.hpp
+++ p2/i82bebey/binary_tree/btree_utils.hpp
@@ -41,18 +41,18 @@ int compute_height (typename BTree<T>::Ref t)
     }*/
     else
     {
-        if (t->has_left() == true)
+        if (has_left() == true)
         {
             aux++;
             if (ret_val < aux) ret_val = aux;
-            compute_height(rootptr->left());
+            compute_height(left());
         }
 
-        if (t->has_right() == true)
+        if (has_right() == true)
         {
             aux++;
             if (ret_val < aux) ret_val = aux;
-            compute_height(rootptr->right());
+            compute_height(right());
         }
         
     }
