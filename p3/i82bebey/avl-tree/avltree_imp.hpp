d0a7bd25d836a1b1f40ff82bec546099034823f8
Youness Bettahi Berdai <48675571+yunesss00@users.noreply.github.com>
2021-04-18 21:14:52 +0200

    Update avltree_imp.hpp

--- p3/i82bebey/avl-tree/avltree_imp.hpp
+++ p3/i82bebey/avl-tree/avltree_imp.hpp
@@ -32,10 +32,7 @@ AVLTNode<T>::AVLTNode (T const& it, AVLTNode<T>::Ref parent,
     item_(it), parent_(parent), left_(left), right_(right), height_(0)
 {
     //TODO
-    item_ = it;
-    parent_ = parent;
-    left_ = left;
-    right_ = right;
+    
     //
     assert(check_height_invariant());
 }
