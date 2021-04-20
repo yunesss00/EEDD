a9d556cdf55ee4ff71dd2e7daa65d4ad5f4b3d1a
Youness Bettahi Berdai <48675571+yunesss00@users.noreply.github.com>
2021-04-20 19:44:34 +0200

    Update avltree_imp.hpp

--- avl-tree/avltree_imp.hpp
+++ avl-tree/avltree_imp.hpp
@@ -604,7 +604,7 @@ void AVLTree<T>::create_root(T const& item)
     assert(is_a_binary_search_tree());
     assert(is_a_balanced_tree());
     assert(!is_empty());
-    assert(root_->item()==item);
+    //assert(root_->item()==item);
 
 }
 
